a307104568e797f2b214f2e0d63c1fc911c3f378
barkin <63337752+barkinadiguzel@users.noreply.github.com>
2025-12-27 22:35:15 +0300

    Create config.py

---
+++ src/config.py
@@ -0,0 +1,4 @@
+PATCH_SIZE = 56       # Patch size S x S
+P_HIDE = 0.5          # Probability of hiding a patch
+MEAN_PIXEL = [0.485, 0.456, 0.406]  # Dataset mean RGB
+CAM_THRESHOLD = 0.2   # Threshold for CAM to create bounding box
